c64821a9f9b6e12d730a7bd09c32cd63be994f50
SanjayGC07 <sanjaygc@Sanjays-MacBook-Pro.local>
2023-02-20 17:06:46 -0800

    good commit

---
+++ src/test/java/DemoTestNGclass.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class DemoTestNGclass {
+
+    @Test
+    public void Test()
+    {
+        System.out.println("this is example of TestNG");
+    }
+}
